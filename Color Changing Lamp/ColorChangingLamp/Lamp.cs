cf8e881be5fb02a5c6334fd8a3711491431297a1
Inella12138 <141432262+Inella12138@users.noreply.github.com>
2023-08-14 13:51:00 +0800

    color changing lamp

---
+++ Color Changing Lamp/ColorChangingLamp/Lamp.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorChangingLamp
+{
+    public class Lamp
+    {
+        private bool isON;
+        private string color;
+        private int sel;
+        private string[] colorsel = { "red", "green", "blue" };
+    
+        public Lamp() 
+        {
+            isON = false;
+            color = colorsel[0];
+            sel = 0;
+        }
+
+        public bool isLampOn() { return isON; }
+
+        public void TurnOn()
+        {
+            if (!isLampOn())
+            {
+                isON = true;
+            }
+            color = colorsel[sel];
+            sel++;
+            if (sel == 3) { sel = 0; }
+        }
+
+        public void TurnOff()
+        {
+            if (isLampOn()) 
+            {
+                isON = false;
+            }
+        }
+
+        public string showCurrentColor()
+        {
+            return color;
+        }
+    }
+}
