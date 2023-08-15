909408e4b2524f770dcf20315dee305cb65b5fca
Inella12138 <141432262+Inella12138@users.noreply.github.com>
2023-08-15 11:22:50 +0800

    add some comments

--- A Random List/RandomList/Program.cs
+++ A Random List/RandomList/Program.cs
@@ -7,18 +7,40 @@ int low;
 int high;
 bool flag;
 
+//Initialization
 Console.Write("Please enter the number of random numbers you want to generate: ");
 n = Convert.ToInt32(Console.ReadLine());
-RandomList X = new RandomList(n);
 
-Console.WriteLine("Please enter the range of numbers you want to generate: ");
-low = Convert.ToInt32(Console.ReadLine());
-high = Convert.ToInt32(Console.ReadLine());
+RandomList X = new RandomList(n);//initialize X
 
-flag = X.Generate(n, low, high);
-if (!flag) { Console.WriteLine("Please check the range!"); }
-else
+//Get the range from user
+while (true)
 {
+    Console.WriteLine("Please enter the range of numbers you want to generate: ");
+    low = Convert.ToInt32(Console.ReadLine());
+    high = Convert.ToInt32(Console.ReadLine());
+
+    flag = X.Generate(n, low, high);//Generate returns a bool while generating numbers
+    if (!flag) { Console.WriteLine("Please check the range!"); }
+    else { break; }//ask users to enter the range until [low < high]
+}
+
+//Generate and Print
+while (true)
+{
+    Console.WriteLine("Please enter the seed for the random number array(-10 - 10): ");
+    if (int.TryParse(Console.ReadLine(), out seed))//if the input is not an integer, program ends.
+    {
+        X.Seed = seed;
+        X.Generate(n, low, high);
+        X.Show();
+        Console.WriteLine("The average value of the random number array is: " + X.Mean());
+        Console.WriteLine("The seed of the random array is: " + X.Seed);
+        Console.WriteLine();
+    }
+    else { break; }
+}
+//只要求seed=5和seed=-8可以参考下列代码
     /*X.Seed = 5;
     X.Generate(n, low, high);
     X.Show();
@@ -29,18 +51,3 @@ else
     X.Show();
     Console.WriteLine("The average value of the random number array is: " + X.Mean());
     Console.WriteLine("The seed of the random array is: " + X.Seed);*/
-    while (true)
-    {
-        Console.WriteLine("Please enter the seed for the random number array(-10 - 10): ");
-        if (int.TryParse(Console.ReadLine(), out seed))
-        {
-            X.Seed = seed;
-            X.Generate(n, low, high);
-            X.Show();
-            Console.WriteLine("The average value of the random number array is: " + X.Mean());
-            Console.WriteLine("The seed of the random array is: " + X.Seed);
-            Console.WriteLine();
-        }
-        else { break; }
-    }
-}
--- A Random List/RandomList/RandomList.cs
+++ A Random List/RandomList/RandomList.cs
@@ -11,11 +11,13 @@ class RandomList
     private int[] randomArray;
     private int seed;
 
+    //Constructor
     public RandomList(int n)
     {
         randomArray = new int[n];
     }
 
+    //Seed attribute being implemented as a C# Property
     public int Seed
     {
         get { return seed; }
@@ -30,14 +32,14 @@ class RandomList
 
     public bool Generate(int n, int low, int high)
     {
-        bool flag = false;
-        Random rnd = new Random(seed);
+        bool flag = false;//to see if [low < high]
+        Random rnd = new Random(seed);//generate random numbers with given seed
         if (low < high)
         {
             flag = true;
             for (int i = 0; i <n; i++)
             {
-                randomArray[i] = rnd.Next(low, high);
+                randomArray[i] = rnd.Next(low, high);//store numbers in array
             }
             return flag;
         }
