8e808b40a9adca0e2ca41ebb7277c8a960cd1f06
Inella12138 <141432262+Inella12138@users.noreply.github.com>
2023-08-15 11:02:03 +0800

    Random List

---
+++ A Random List/RandomList/Program.cs
@@ -0,0 +1,46 @@
+﻿//Main
+using System.CodeDom.Compiler;
+
+int n = 0;
+int seed;
+int low;
+int high;
+bool flag;
+
+Console.Write("Please enter the number of random numbers you want to generate: ");
+n = Convert.ToInt32(Console.ReadLine());
+RandomList X = new RandomList(n);
+
+Console.WriteLine("Please enter the range of numbers you want to generate: ");
+low = Convert.ToInt32(Console.ReadLine());
+high = Convert.ToInt32(Console.ReadLine());
+
+flag = X.Generate(n, low, high);
+if (!flag) { Console.WriteLine("Please check the range!"); }
+else
+{
+    /*X.Seed = 5;
+    X.Generate(n, low, high);
+    X.Show();
+    Console.WriteLine("The average value of the random number array is: " + X.Mean());
+    Console.WriteLine("The seed of the random array is: " + X.Seed);
+    X.Seed = -8;
+    X.Generate(n, low, high);
+    X.Show();
+    Console.WriteLine("The average value of the random number array is: " + X.Mean());
+    Console.WriteLine("The seed of the random array is: " + X.Seed);*/
+    while (true)
+    {
+        Console.WriteLine("Please enter the seed for the random number array(-10 - 10): ");
+        if (int.TryParse(Console.ReadLine(), out seed))
+        {
+            X.Seed = seed;
+            X.Generate(n, low, high);
+            X.Show();
+            Console.WriteLine("The average value of the random number array is: " + X.Mean());
+            Console.WriteLine("The seed of the random array is: " + X.Seed);
+            Console.WriteLine();
+        }
+        else { break; }
+    }
+}
---
+++ A Random List/RandomList/RandomList.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class RandomList
+{
+    private int[] randomArray;
+    private int seed;
+
+    public RandomList(int n)
+    {
+        randomArray = new int[n];
+    }
+
+    public int Seed
+    {
+        get { return seed; }
+        set
+        {
+            if (value >= -10 && value <= 10)
+            {
+                seed = value;
+            }
+        }
+    }
+
+    public bool Generate(int n, int low, int high)
+    {
+        bool flag = false;
+        Random rnd = new Random(seed);
+        if (low < high)
+        {
+            flag = true;
+            for (int i = 0; i <n; i++)
+            {
+                randomArray[i] = rnd.Next(low, high);
+            }
+            return flag;
+        }
+        else { return flag; }
+    }
+
+    public void Show()
+    {
+        Console.Write("The random numbers are:");
+        for (int i = 0; i < randomArray.Length; i++)
+        {
+            Console.Write(randomArray[i]+"\t");
+        } 
+        Console.WriteLine();
+    }
+
+    public float Mean()
+    {
+        float ave = 0;
+        for(int i = 0;i < randomArray.Length;i++)
+        {
+            ave += randomArray[i] / randomArray.Length;
+        }
+        return ave;
+    }
+}
+
